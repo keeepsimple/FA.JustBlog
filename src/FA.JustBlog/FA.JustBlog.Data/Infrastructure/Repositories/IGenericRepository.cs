9c10591b9ed98aa2888e3e39bb182c680e754dda
QuangNV33 <quangbeo199x@gmail.com>
2021-08-03 07:40:12 +0700

    Update web mvc

---
+++ src/FA.JustBlog/FA.JustBlog.Data/Infrastructure/Repositories/IGenericRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace FA.JustBlog.Data.Infrastructure.Repositories
+{
+    public interface IGenericRepository<TEntity>
+    {
+        /// <summary>
+        /// Get a <typeparamref name="TEntity"/> by id with async
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<TEntity> GetByIdAsync(Guid id);
+
+        TEntity GetById(Guid id);
+
+        /// <summary>
+        /// Add an <typeparamref name="TEntity"/>
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        void Add(TEntity entity);
+
+        void Add(IEnumerable<TEntity> entities);
+
+        /// <summary>
+        /// Delete an <typeparamref name="TEntity"/>
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        void Delete(TEntity entity, bool isHardDelete = false);
+
+        void Delete(IEnumerable<TEntity> entities, bool isHardDelete = false);
+
+        void Delete(Expression<Func<TEntity, bool>> where, bool isHardDelete = false);
+
+        IQueryable<TEntity> GetQuery();
+
+        IQueryable<TEntity> GetQuery(Expression<Func<TEntity, bool>> where);
+
+        IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, 
+            IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "", bool canLoadDeleted = false);
+
+        /// <summary>
+        /// Update an <typeparamref name="TEntity"/>
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        void Update(TEntity entity);
+    }
+}
---
+++ src/FA.JustBlog/FA.JustBlog.Services/ITagServices.cs
@@ -0,0 +1,18 @@
+﻿using FA.JustBlog.Models.Common;
+using FA.JustBlog.Services.BaseServices;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FA.JustBlog.Services
+{
+    public interface ITagServices : IBaseService<Tag>
+    {
+        Tag GetTagByUrlSlug(string urlSlug);
+
+        Task<Tag> GetTagByUrlSlugAsync(string urlSlug);
+
+        Task<IEnumerable<Tag>> GetPopularTagAsync(int size);
+
+        Task<IEnumerable<Tag>> GetPopularTags(int size);
+    }
+}
